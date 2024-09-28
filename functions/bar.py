fea125528024911c6dc8e496a5075e8b32ccf4a9
kittycattoys <ninedynamics@gmail.com>
2024-09-27 18:18:16 -0600

    T

--- functions/bar.py
+++ functions/bar.py
@@ -25,7 +25,6 @@ def plot_bar_chart(filtered_df, selected_stock_symbol):
             margin=dict(l=0, r=0, t=20, b=0),
             xaxis={'fixedrange':True},
             yaxis={'fixedrange':True},
-            config={'displayModeBar': False},
             height=500,
             modebar=dict(remove=["zoom", "pan", "select2d", "lasso2d", "autoScale", "resetScale", "zoomIn", "zoomOut", "resetViews"])
         )
