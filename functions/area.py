4854cf724061b76a0db440389ec8fec39291910c
kittycattoys <ninedynamics@gmail.com>
2024-09-29 14:39:55 -0600

    T

--- functions/area.py
+++ functions/area.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 def plot_area_chart(df_fact, selected_stock_symbol):
     if not df_fact.empty:
-        # Ensure dt_st is a datetime object
-        df_fact['dt_st'] = pd.to_datetime(df_fact['dt_st'])
+        # Plotting stock prices using Plotly
+        #df_fact['dt_st'] = pd.to_datetime(df_fact['dt_st']).dt.strftime("%b %y").astype(str)
 
         fig = go.Figure()
 
@@ -51,8 +51,8 @@ def plot_area_chart(df_fact, selected_stock_symbol):
 
         # Add rectangles for reference areas
         df_rectangles = pd.DataFrame({
-            'start_date': ["2023-05-19", "2023-01-15"], 
-            'end_date': ["2023-05-22", "2023-01-18"], 
+            'start_date': ["May 19", "Jan 15"], 
+            'end_date': ["May 22", "Jan 18"], 
             'color': ['green', 'red'], 
             'label': ['Reference Area 1', 'Reference Area 2']
         })
@@ -60,8 +60,8 @@ def plot_area_chart(df_fact, selected_stock_symbol):
         # Loop through the dataframe to dynamically add vertical rectangles
         for index, row in df_rectangles.iterrows():
             fig.add_vrect(
-                x0=pd.to_datetime(row['start_date']), 
-                x1=pd.to_datetime(row['end_date']),
+                x0=row['start_date'], 
+                x1=row['end_date'],
                 fillcolor=row['color'], 
                 opacity=0.2,
                 layer="below", 
@@ -70,31 +70,35 @@ def plot_area_chart(df_fact, selected_stock_symbol):
                 annotation_position="top left"
             )
 
-        # Calculate the length of the index
-        len_x = len(df_fact['dt_st'])
-
-        # Set Custom X-axis Padding
-        custom_xaxis_pad = 1
-
         # Customize layout
         fig.update_layout(
-            xaxis=dict(
-                rangemode='tozero',
-                range=[-1 * custom_xaxis_pad, len_x + (custom_xaxis_pad - 1)]
-            ),
-            yaxis=dict(
-                showspikes=True, 
-                spikemode='across', 
-                spikecolor='red', 
-                spikethickness=1,
-                ticklabelposition='inside',
-                automargin=True, 
-            ),
+            #title=f"{selected_stock_symbol} Stock Prices",
+            xaxis_title=None,
+            yaxis_title=None,
             showlegend=False, 
             margin=dict(l=0, r=0, t=30, b=0),
             height=500,
             hovermode='x',
             dragmode=False,
+            yaxis={
+                'showspikes': True, 
+                'spikemode': 'across', 
+                'spikecolor': 'red', 
+                'spikethickness': 1,
+                'ticklabelposition': 'inside',
+                'automargin': True, 
+            },
+            xaxis={
+                'showspikes': True, 
+                'spikemode': 'across', 
+                'spikecolor': 'red', 
+                'spikethickness': 1,
+                'tickmode': 'linear',
+                'tickfont': {'size': 10, 'color': 'grey'},
+                'dtick': 12, 
+                'range': [df_fact['dt_st'].min(), df_fact['dt_st'].max()],
+            },
+            modebar=dict(remove=["zoom", "pan", "select2d", "lasso2d", "autoScale", "resetScale", "zoomIn", "zoomOut", "resetViews"]),
         )
 
         st.plotly_chart(fig)
