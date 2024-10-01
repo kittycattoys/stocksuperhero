db08f9c3b9d4699ee75e1e8361b34422ea2e54fb
kittycattoys <ninedynamics@gmail.com>
2024-09-30 18:46:27 -0600

    G

--- stocksuperhero.py
+++ stocksuperhero.py
@@ -103,11 +103,10 @@ if not st.session_state['authenticated']:
             #demo_button = st.form_submit_button("Demo Login", help="Log in with a demo key.")
             st.subheader(f"Demo Coming Soon")
 
-    
     # Handle button clicks
     if submit_button and user_key_input:
         login_user(user_key_input)  # Regular login with provided key
-    
+        
     #if demo_button:
     #    login_user("key")  # Demo login with hardcoded key
 else:
